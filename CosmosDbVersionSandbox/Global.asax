bb1808bbefd2ce8a11156a8fc7d3345cd3f16b73
Ian Stapleton <ian@geekcubed.org>
2017-11-11 10:41:42 +0000

    Initial commit - default asp.net website with latest WebAPI2 package

---
+++ CosmosDbVersionSandbox/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CosmosDbVersionSandbox.WebApiApplication" Language="C#" %>
---
+++ CosmosDbVersionSandbox/Global.asax.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace CosmosDbVersionSandbox
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            //Remove XML Formatter
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+        }
+    }
+}
